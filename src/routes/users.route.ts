7b0ca554bedce6c4e848879ca4f23f8041c157b6
francienevaz <fran.vfraga@gmail.com>
2022-01-26 17:17:40 -0300

    updated

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,9 +2,14 @@ import { NextFunction, Request, Response, Router } from 'express';
 
 const usersRoute = Router();
 
-usersRoute.get('/', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{userName: 'Fran'}];
     res.status(200).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.sendStatus(200).send({uuid});
 });
 
 export default usersRoute;
