0441c913df0064a0a38e8fc568c64c1ffb443ec4
francienevaz <fran.vfraga@gmail.com>
2022-01-28 17:06:44 -0300

    adding put and delete

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from "./routes/users.route";
 
 const app = express();
 
+//Configurações da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Configurações das Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'I can do it'});
 });
 
+//Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 })
