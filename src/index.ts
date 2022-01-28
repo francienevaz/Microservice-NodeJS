266ccdf04b544b858d396a14e92e5544ce10dc78
francienevaz <fran.vfraga@gmail.com>
2022-01-28 17:19:48 -0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, {Request, Response, NextFunction} from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
@@ -9,10 +10,7 @@ app.use(express.urlencoded({extended: true}));
 
 //Configurações das Rotas
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'I can do it'});
-});
+app.use(statusRoute);
 
 //Inicialização do servidor
 app.listen(3000, () => {
