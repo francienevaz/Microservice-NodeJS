e261497dc98573ca8c910de93068c5321c799242
francienevaz <fran.vfraga@gmail.com>
2022-01-28 15:45:35 -0300

    updated

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response, Router } from 'express';
-
+import {StatusCodes} from 'http-status-codes'
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{userName: 'Fran'}];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.sendStatus(200).send({uuid});
+    res.sendStatus(StatusCodes.OK).send({uuid});
 });
 
 export default usersRoute;
