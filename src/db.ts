1a37532497a5190df5884027cdbd546176838719
francienevaz <fran.vfraga@gmail.com>
2022-01-30 15:31:00 -0300

    updated

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = 'postgres://mivuvsdl:xbx4hePxjO5dDky7cSUH_ykcV80DDbBg@motty.db.elephantsql.com/mivuvsdl';
+const connectionString = '';
 
 const db = new Pool({connectionString});
 
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,5 +1,6 @@
 import db from '../db';
 import User from '../models/user.models';
+import DatabaseError from '../models/error/database.error';
 
 
 
@@ -16,18 +17,69 @@ class UserRepository {
     }
 
     async findById(uuid: string): Promise<User> {
-        const query = `
-            SELECT uuid, username
+        try {
+            const query = `
+                        SELECT uuid, username
+                        FROM application_user
+                        WHERE uuid = $1
+                    `;
+                    const values = [uuid];
+
+                    const {rows} = await db.query<User>(query, values);
+                    const [user] = rows;
+
+                    return user;
+            } catch (error) {
+                    throw new DatabaseError('Error na consulta por ID', error);
+            }
+
+        
+    }
+
+    async create(user: User): Promise<string> {
+        const script = `
+            INSERT INTO application_user (
+                username,
+                password
+            )
+            VALUES ($1, crypt($2, 'my_salt'))
+            RETURNING uuid
+        `;
+
+        const values =[user.username, user.password];
+
+        const {rows} = await db.query<{uuid: string}>(script, values)
+        const [newUser] = rows;
+        return newUser.uuid;
+
+    }
+
+    async update(user: User): Promise<void> {
+        const script = `
+            UPDATE application_user
+            SET
+                username = $1,
+                password = crypt($2, 'my_salt')
+            WHERE uuid = $3            
+        `;
+
+        const values =[user.username, user.password, user.uuid];
+
+        await db.query(script, values);
+    }
+
+   async remove(uuid: string): Promise<void> {
+       const cript = `
+            DELETE
             FROM application_user
             WHERE uuid = $1
-        `;
-        const values = [uuid];
+       `;
 
-        const {rows} = await db.query<User>(query, values);
-        const [user] = rows;
+       const values = [uuid];
 
-        return user;
-    }
+       await db.query(cript, values);
+       
+   }
 
 
 }
